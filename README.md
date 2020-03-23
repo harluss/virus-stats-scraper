04306891b985160bb9072c98de2da388f81b50d6
harluss <harluss@gmail.com>
2020-03-23 00:28:22 +0000

    Script added, README updated

--- README.md
+++ README.md
@@ -1,1 +1,20 @@
-# virus-stats-scraper
+# Node and Puppeteer Web Scraper
+Uses Node.js and Puppeteer to scrape and display stats about the current epidemic for requested countries.
+
+### Setup
+Install Node packages with:
+```
+npm install
+```
+### Usage
+```
+node scraper <country> <country> <country>
+```
+It will accept a list of countries as arguments (or default to `Ireland` if no country is provided), scrape the stats of [Worldometers.info](https://www.worldometers.info/coronavirus/#countries) and display them in the terminal in a table format.
+
+### Note:
+This script was made to make it easier for me to get the information I want and learn something new in the process (task automation and web scraping with Node and Puppeteer), not to generate 'clicks' by jumping on this popular yet difficult topic. 
+### Packages
+```
+"puppeteer": "^2.1.1"
+```
---
+++ scraper.js
@@ -0,0 +1,50 @@
+const puppeteer = require('puppeteer');
+
+(async () => {
+  const countries = process.argv.length > 2 ? process.argv.slice(2) : ['Ireland'];
+
+  const browser = await puppeteer.launch();
+  const page = await browser.newPage();
+  await page.goto('https://www.worldometers.info/coronavirus/#countries');
+
+  const [results, errors] = await page.evaluate((countries) => {
+    const table = document.querySelector('table#main_table_countries_yesterday');
+
+    const tableHeaders = table.querySelector('thead > tr').children;
+    const headers = [...tableHeaders].map(h => h.innerText);
+
+    const rows = table.querySelectorAll('td');
+    let bigData = [];
+    let noData = [];
+
+    countries.forEach(country => {
+      const targetRow = Array.from(rows).find(r => r.innerText.toLowerCase() === `${country}`.toLowerCase());
+
+      if (!targetRow) {
+        noData.push(` ${country}`);
+        return;
+      }
+
+      const dataRows = targetRow.parentElement.children;
+      const values = [...dataRows].map(r => r.innerText.trim());
+
+      let dataCountry = {};
+
+      headers.forEach((h, i) => {
+        dataCountry[h] = values[i];
+      });
+
+      bigData.push(dataCountry);
+    });
+
+    return [bigData, noData];
+  }, countries);
+
+  console.table(results);
+
+  if (errors.length > 0) {
+    console.log(`ERROR: No data for:${errors}!`);
+  }
+
+  await browser.close();
+})();
